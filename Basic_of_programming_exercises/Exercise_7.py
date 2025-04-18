f4fd23135426c02ccd126f235986b86f4c56b26e
Prabin Sapkota <sapkotaprabin8001@gmail.com>
2025-04-18 14:06:12 +0300

    Fixed code

--- Basic_of_programming_exercises/Exercise_7.py
+++ Basic_of_programming_exercises/Exercise_7.py
@@ -2,7 +2,7 @@ def positives_sum(ls):
     #Condition for positive sum:=
     return sum(num for num in ls if num > 0)
 
-# Testing some cases
+# Testing some cases are for positive sums
 print(positives_sum([1, 21, 3, 40, 5]))  
 print(positives_sum([1, -2, 3]))         
 print(positives_sum([-1, -2, -3]))       
