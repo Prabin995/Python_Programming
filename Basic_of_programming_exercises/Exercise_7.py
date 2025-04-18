7c04349ce47c633746ede09e9ed7e265d857d046
Prabin Sapkota <sapkotaprabin8001@gmail.com>
2025-04-18 14:02:21 +0300

    Added new Exercise file

---
+++ Basic_of_programming_exercises/Exercise_7.py
@@ -0,0 +1,8 @@
+def positives_sum(ls):
+    #Condition for positive sum:=
+    return sum(num for num in ls if num > 0)
+
+# Testing some cases
+print(positives_sum([1, 21, 3, 40, 5]))  
+print(positives_sum([1, -2, 3]))         
+print(positives_sum([-1, -2, -3]))       
