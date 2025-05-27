7989e0004debc09f625b908418c81adaf1d3cb8c
Prabin Sapkota <sapkotaprabin8001@gmail.com>
2025-05-27 21:25:25 +0300

    added new task on prevous folder

--- Basic_of_programming_exercises/functin.py
+++ Basic_of_programming_exercises/functin.py
@@ -20,3 +20,21 @@ def check_even_odd(number):
 num = int(input("Enter a number:  "))
 result = check_even_odd(num)
 print(f"{num} is {result}")
+
+
+# Write a function called is_positive_negative_zero(number) that:
+#Returns "Positive" if the number is greater than 0
+#Returns "Negative" if the number is less than 0
+#Returns "Zero" if the number is 0
+
+def is_positive_negative_zero(number):
+    if number>0:
+        return "Positive number!"
+    elif number ==0:
+        return "Zero!"
+    else:
+        return "Negative number!"
+    
+num = int(input("Enter a number:    "))
+result = is_positive_negative_zero(num)
+print(f"{num} is {result}")
