c2a85a126e7fcba5f96f2ad23d1c4f6a53c91b7a
Prabin Sapkota <sapkotaprabin8001@gmail.com>
2025-05-27 06:53:53 +0300

    Added new task

---
+++ Basic_of_programming_exercises/Control_flow_ex.py
@@ -0,0 +1,10 @@
+#Write the Even or Odd program.
+
+number = int(input("Please enter a number!   "))
+
+if number%2==0:
+    print(number, "is an even number.")
+
+else:
+    print(number, "is an odd number.")
+   
