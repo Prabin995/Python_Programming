15d8506b4b79facc6e017b393981dd1c0ac982d0
Prabin Sapkota <sapkotaprabin8001@gmail.com>
2025-05-28 20:20:34 +0300

    Added new task

---
+++ Basic_of_programming_exercises/Loop.py
@@ -0,0 +1,35 @@
+#Loops help you repeat tasks without writing code again and again
+#for loop
+for i in range(5):
+    print(i)
+
+#while loop
+count = 0
+while count<5:
+    print(count)
+    count +=1
+
+#Lists in Python
+#A list stores multiple items in a single variable.
+numbers = [4, 7, 2, 9]
+print(numbers[0])
+print(len(numbers))
+
+for num in numbers:
+    print(num)
+
+#Write a program to:
+#Ask the user how many numbers they want to enter.
+#Store those numbers in a list.
+#Use a loop to find the total sum.
+#Print the list and the sum.
+
+Wanted_number = int(input("How many number do you want to enter?  "))
+List = []
+for i in range(Wanted_number):
+    num = int(input(f" Enter number {i+1}:  "))
+    List.append(num)
+
+total = sum(List)
+print("List:",List)
+print("sum:",total)
