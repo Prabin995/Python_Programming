e0bede62ce8adc28c575344133e8474ded1d1f69
Prabin Sapkota <sapkotaprabin8001@gmail.com>
2025-05-29 10:12:19 +0300

    fixed the code

--- Basic_of_programming_exercises/triangle_pattern.py
+++ Basic_of_programming_exercises/triangle_pattern.py
@@ -1,9 +1,12 @@
+#for making triangle of numbers 
 rows = int(input("Enter the number of rows:  "))
 for i in range (1, rows +1):
     for j in range(1,i+1):
         print(j, end=" ")
     print()
 
+
+#reversing this triangle.
 rows1 = int(input("Enter the number of rows:  "))
 for i in range (rows1, 0, -1):
     for j in range(1,i+1):
